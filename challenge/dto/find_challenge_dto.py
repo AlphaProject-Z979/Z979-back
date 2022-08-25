1ed057323ec759a246a46fcb840c8e21a49d8aee
Youngho <cyh6099@gmail.com>
2022-08-25 14:12:29 +0900

    feat/챌린지 CRUD(User관련 추가 필요)

---
+++ challenge/dto/find_challenge_dto.py
@@ -0,0 +1,7 @@
+class FindChallengeTypeDto:
+
+    # TODO: 챌린지 썸네일 추가
+    def __init__(self, challenge_name, challenge_description):
+        self.challenge_name = challenge_name
+        self.challenge_description = challenge_description
+
---
+++ challenge/dto/find_challenge_type_feed_dto.py
@@ -0,0 +1,7 @@
+# content, images, hashtags, userinfo
+
+class FindChallengeFeedDto:
+    # TODO : images, userinfo
+    def __init__(self,content, hashtags):
+        self.content = content
+        self.hashtags = hashtags
